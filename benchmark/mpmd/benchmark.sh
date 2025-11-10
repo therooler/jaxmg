ef40177c689068592751ef2b3f57d62bfb163365
Roeland Wiersema <roel.wier@proton.me>
2025-11-10 11:54:13 -0500

    Add benchmarking

--- benchmark/mpmd/benchmark.sh
+++ benchmark/mpmd/benchmark.sh
@@ -5,7 +5,7 @@ HOSTS=($(scontrol show hostnames "$SLURM_JOB_NODELIST"))
 echo $HOSTS
 MASTER=${HOSTS[0]}
 for i in $range; do
-  python -u mp_test.py "$MASTER:10001" $i $num_processes > /tmp/toy_$i.out &
+  python -u benchmark.py "$MASTER:10001" $i $num_processes > /tmp/toy_$i.out &
 done
 
 wait
